53cf8192eb3a0e03fe2975491481ed55c6bb99ac
James Li <lhj1982@gmail.com>
2019-11-20 22:36:50 +0800

    updated db backup script

--- scripts/db/dbexport.sh
+++ scripts/db/dbexport.sh
@@ -6,19 +6,28 @@ if [ ! $1 ]; then
 fi
 db=$1
 out_dir=$2
+DATE=`date +"%Y-%m-%d_%H_%M_%S"`
+DUMPFILENAME=${db}_${DATE}.tar.gz
+UN=admin
+PW=12wed98uh56yhbv
+
 if [ ! $out_dir ]; then
-        out_dir="./out"
+        out_dir="./backup"
 else
         mkdir -p $out_dir
 fi
 
+echo test
 tmp_file="fadlfhsdofheinwvw.js"
-echo "print('_ ' + db.getCollectionNames())" > $tmp_file
-cols=`mongo $db $tmp_file | grep '_' | awk '{print $2}' | tr ',' ' '`
+echo -e "use ${db}\nprint('_ ' + db.getCollectionNames())" > $tmp_file
+cols=`mongo -u ${UN} -p ${PW} < $tmp_file | grep '_' | awk '{print $2}' | tr ',' ' '`
 for c in $cols
 do
-    mongoexport -h 127.0.0.1 -d $db -c $c -o "$out_dir/exp_${db}_${c}.json"
+    mongoexport -h 127.0.0.1 -d $db -u ${UN} -p ${PW} -c $c -o "$out_dir/exp_${db}_${c}.json" --authenticationDatabase=admin
 done
-rm $tmp_file
+tar -czvf ${DUMPFILENAME} ${out_dir} || exit 1
+aws s3 cp ${DUMPFILENAME} s3://jbs-backup --region ap-southeast-1 || exit 2
+rm ${DUMPFILENAME} || exit 3
+rm $tmp_file || exit 4
 
 exit 0
